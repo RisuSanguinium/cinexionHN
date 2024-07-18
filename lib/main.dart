b782a2ec860fb4a146ba7f9e32b00aedbac07b3c
RisuSanguinium <jargrubio@gmail.com>
2024-07-17 18:07:27 -0600

    Prueba 2

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Cinexion',
+      title: 'Cinexion Prueba de push para',
       theme: ThemeData(
         // This is the theme of your application.
         //
